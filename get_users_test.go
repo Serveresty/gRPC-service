a1711d89e144cf4a3d473dc999ee8dc43c561d98
Serveresty <cs.go.12228@gmail.com>
2023-11-24 05:11:47 +0300

    created test for GetUsers with auth

--- get_users_test.go
+++ get_users_test.go
@@ -11,6 +11,7 @@ import (
 	"testing"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/test/bufconn"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
@@ -39,7 +40,13 @@ func TestGetUsers(t *testing.T) {
 		return lis.Dial()
 	}
 
-	conn, err := grpc.DialContext(context.Background(), "", grpc.WithContextDialer(dialer), grpc.WithInsecure())
+	requestToken := new(service.AuthToken)
+	requestToken.Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dpbiI6Imhpcm8ifQ.PPSb0vo-Y-OG2teShWxgI0bICn3MthALuvHpPbUvj3E"
+	ctx := context.Background()
+	md := metadata.New(map[string]string{"authorization": requestToken.Token})
+	newCtx := metadata.NewOutgoingContext(ctx, md)
+
+	conn, err := grpc.DialContext(newCtx, "", grpc.WithContextDialer(dialer), grpc.WithInsecure())
 	if err != nil {
 		t.Fatalf("grpc.DialContext %v", err)
 	}
@@ -93,7 +100,7 @@ func TestGetUsers(t *testing.T) {
 				Email:     tt.Email,
 				DateFrom:  tt.DateFrom,
 				DateTo:    tt.DateTo}}
-			res, err := c.GetInfoAboutUser(context.Background(), req)
+			res, err := c.GetInfoAboutUser(newCtx, req)
 			if err != nil {
 				t.Errorf("GetUserTest(%v) got an error: %v", tt.NameTest, err)
 			}
